6bb0aabe44ae44f6f43db2c8efe83bf253fca4ea
Jordan Max <jordmax12@gmail.com>
2016-05-13 17:26:15 -0500

    sending data from controller to child view

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-//Create new component and it should produce html
-
-"use strict";
-
-import SearchBar from './components/search_bar.js';
-
-const App = () => {
-    //JSX - subset of javascript, lets us write "html" but really its javascript
-    //webpack and babel will convert our "html" to real "html"
-    return (
-        <div>
-        <SearchBar/>
-        </div>
-    );
-}
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
 
 const YTUBE_API_KEY = 'AIzaSyArt90VQ81DKQt6sEuTBugVCzE_kQOTEY4';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { videos: [] };
+        
+        YTSearch({key: YTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+           this.setState({ videos });
+           //this.setState({ videos: videos });
+        });
+    }
+    
+    render () {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+};
+//JSX - subset of javascript, lets us write "html" but really its javascript
+//webpack and babel will convert our "html" to real "html"
+
 //take this components generated html and put it on the page (dom)
 ReactDOM.render(<App />, document.querySelector('.container'));
